96548907520dafcfb1d2d59be393e8772c39627b
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2024-05-17 16:14:10 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ htmlFiles.forEach((file) => {
 });
 
 export default defineConfig({
-  base: '/vite-landing-template',
+  base: '/BookAnim',
   root: 'src',
   publicDir: '../public',
   plugins: [
